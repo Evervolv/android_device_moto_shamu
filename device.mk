aeb9f6a8d5153dfe0af084d4815a97c9daa1f797
dhacker29 <dhackerdvm@gmail.com>
2015-05-28 00:13:14 -0400

    Set WiFi calling props
    Change-Id: I8db045bbc80fba70950b665640761bb4a2d736cb

--- device.mk
+++ device.mk
@@ -271,6 +271,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.alt_mbn_name=tmo_alt.mbn \
     ro.com.android.prov_mobiledata=false
 
+# WiFi calling
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.radio.ignore_ims_wlan=1 \
+    persist.radio.data_con_rprt=1
+
 # Rich Communications Service is disabled in 5.1
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.rcs.supported=0
