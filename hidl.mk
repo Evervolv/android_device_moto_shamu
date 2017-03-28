76611bb91b9dfdaa8e022478d0fd5b089368d019
Steven Moreland <smoreland@google.com>
2017-03-27 17:30:56 -0700

    Sensors hal: toggled -> hwbinder
    Test: (sanity) photosphere works
    Test: (sanity) screen rotation works
    Test: (sanity) VR works
    Test: lshal shows expected output
    Test: Sensors VTS passes
    Bug: 34274385
    Bug: 36955154
    Change-Id: Ia9f12f73be6c738aa6f5d745032befc19e3da01d
    (cherry picked from commit ffccf8567516016647d51946faa8910ea4540768)

--- hidl.mk
+++ hidl.mk
@@ -93,7 +93,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 # Thermal
 PRODUCT_PACKAGES += \
