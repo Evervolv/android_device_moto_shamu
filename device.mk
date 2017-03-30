5682dd2783c0564d27fcb2eb3472f20b544fe38a
Chia-I Wu <olv@google.com>
2017-03-30 11:59:36 -0700

    Enable binderized graphics allocator
    On fugu, SurfaceFlinger must be the graphics allocator.  Set
    SF_START_GRAPHICS_ALLOCATOR_SERVICE for that purpose.
    
    Bug: 36462585
    Test: YouTube, Play Movies
    Change-Id: I3db00645257566a32dc3dd6c2d0e2ca44b16a1dd

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
     device/moto/shamu/fstab.shamu:root/fstab.shamu \
     device/moto/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc
 
+# Vendor Interface Manifest
+PRODUCT_COPY_FILES += \
+    device/moto/shamu/manifest.xml:system/vendor/manifest.xml
+
 # Input device files for shamu
 PRODUCT_COPY_FILES += \
     device/moto/shamu/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
@@ -152,6 +156,10 @@ PRODUCT_PACKAGES += \
     memtrack.msm8084 \
     libqdutils \
     libqdMetaData
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl
 
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
