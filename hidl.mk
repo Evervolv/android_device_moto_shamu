7837e31322edefe3890b67259d5555f5d3b5ab17
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    Adding Audio HAL V5: Direction API
    Bug: 119137468
    Test: With testbed app
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- hidl.mk
+++ hidl.mk
@@ -16,8 +16,8 @@
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl
 
 # Bluetooth
 PRODUCT_PACKAGES += \
