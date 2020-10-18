ProtonAOSP/android_device_google_wahoo

Add device compatibility matrix.
Test: device boots
Bug: 37954458
Change-Id: I437a4506c17908e3107641bf6f1f0b25e4b17248

BoardConfig.mk
@@ -143,3 +143,4 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # Vendor Interface Manifest
 DEVICE_MANIFEST_FILE := device/google/wahoo/manifest.xml
+DEVICE_MATRIX_FILE := device/google/wahoo/compatibility_matrix.xml

Evervolv/android_device_moto_shamu

BoardConfig.mk
@@ -118,6 +118,7 @@ TARGET_KERNEL_SOURCE := kernel/moto/shamu
 
 # Manifests
 DEVICE_MANIFEST_FILE := device/moto/shamu/manifest.xml
+DEVICE_MATRIX_FILE := device/moto/shamu/compatibility_matrix.xml
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true